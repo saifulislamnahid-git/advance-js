aee5c3130b5c02a232b4143f274300e0cde55a46
Saiful islam <saifulkapasia23@gmail.com>
2021-02-02 14:31:44 +0600

    Added truthy of falsy

---
+++ true-false.js
@@ -0,0 +1,23 @@
+// const name = "Saiful";
+let name = NaN;
+if (name) {
+    console.log('Condition is true');
+}else{
+    console.log('Condition is false');
+}
+
+// 0 is conditionally false
+// any type of number is conditionally true
+// String are truthy without emty sting.
+// Emty string are falsy
+
+// False
+// 0;
+// '';
+// undefined;
+// null;
+// NaN;
+// false;
+// Truthy
+// '0', ' ', [], true
+
